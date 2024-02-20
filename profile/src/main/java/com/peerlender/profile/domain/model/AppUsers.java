b1980c9df5e027957cab622b9c5d635b4642443f
Troy.Andati <andatisiro@gmail.com>
2024-02-20 15:26:45 +0300

    appusers

--- profile/src/main/java/com/peerlender/profile/domain/model/AppUsers.java
+++ profile/src/main/java/com/peerlender/profile/domain/model/AppUsers.java
@@ -2,7 +2,6 @@ package com.peerlender.profile.domain.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
