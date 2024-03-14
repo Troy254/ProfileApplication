28d06fd060c43a07a279d1f9e8128734564ba757
Troy.Andati <andatisiro@gmail.com>
2024-03-14 09:17:37 +0300

    EventHandler

--- profile/src/main/java/com/peerlender/profile/domain/event/UserRegisteredEventHandler.java
+++ profile/src/main/java/com/peerlender/profile/domain/event/UserRegisteredEventHandler.java
@@ -22,7 +22,7 @@ public class UserRegisteredEventHandler {
   }
 
   public void handleUserRegistration(String userDetails) {
-    AppUsers appUsers = GSON.fromJson(userDetails, AppUsers.class);
+    AppUsers appUsers = GSON.fromJson(userDetails,AppUsers.class);
     LOGGER.info("user {} registered", appUsers.getUsername());
     usersRepository.save(appUsers);
   }
